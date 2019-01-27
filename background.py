639283b7f6369b224ba5334f4dbd1e1c4e158dcd
carlmarks203 <45235909+carlmarks203@users.noreply.github.com>
2019-01-27 10:31:51 +0300

    Добавление генерации облаков на BG

--- background.py
+++ background.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 running = True
 pygame.init()
@@ -17,19 +18,38 @@ class Background(pygame.sprite.Sprite):
         self.rect.x = 0
         self.rect.y = inputed
 
-
     def update(self):
-        if self.rect.y == 801:
+        if self.rect.y == 800:
             self.rect.y = -800
         self.rect = self.rect.move(0, 1)
 
 
-bk_1 = Background(sky, -800)
-bk_2 = Background(sky, 0)
+class Clouds(pygame.sprite.Sprite):
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = pygame.image.load('data/cloud.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0, 350)
+        self.rect.y = random.randrange(-800, -75)
+
+    def update(self):
+        if self.rect.y == 801:
+            self.rect.x = random.randrange(-50, 375)
+            self.rect.y = random.randrange(-1500, -75)
+        self.rect = self.rect.move(0, 1)
+
+
+bg_1 = Background(sky, -800)
+bg_2 = Background(sky, 0)
+cds = []
+for i in range(9):
+    cds.append(Clouds(sky))
 
 while running:
     screen.fill((0, 0, 0))
-    bk_1.update()
-    bk_2.update()
+    bg_1.update()
+    bg_2.update()
+    for i in cds:
+        i.update()
     sky.draw(screen)
     pygame.display.flip()
